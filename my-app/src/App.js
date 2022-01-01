60dec79d363b94301f4086c718391b4c5765ba9b
Nguyễn Hoàng Minh <minhnh2298@MacBook-Air-cua-Nguyen.local>
2022-01-01 12:49:30 +0700

    fix link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import BoardFabricNew from './components/BoardFabricNew';
 //'ws://' + '127.0.0.1:8000' + '/ws/websocket/'
 function App() {
   const webSocket = useRef(null)
-  webSocket.current = new WebSocket("ws://" + "localhost:8000" + "/")
+  webSocket.current = new WebSocket("ws://localhost:8000/")
 
   webSocket.current.onopen = () => {
     console.log('WebSocket open');
