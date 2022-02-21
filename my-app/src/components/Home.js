632891c67339e4f86d50e1f455bc386fc1f8ec91
Minh Hoang <hoangminh98.hn2@gmail.com>
2022-02-22 00:21:07 +0700

    aa

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -15,17 +15,20 @@ function Home() {
 
     return (
         <div className="bg-cover bg-center bg-gray-600 h-screen">
-            <div className='absolute place-items-center h-3/4 w-96 bg-gray-700 left-4/10 top-28 rounded-3xl'>
-                <p className='text-center text-3xl top-5 m-7  text-white '>
+            <div className='absolute place-items-center h-2/5 w-378px left-39% bg-cyan-300 top-29% rounded-3xl'>
+                <p className='text-center text-3xl top-5 m-7  text-black '>
                     Draw Real Time
                 </p>
-                <div className='mt-24 text-3xl p-4 text-white'>
+                <div className=' mt-8 text-3xl p-4 text-black'>
                     <label htmlFor='roomId'>Room ID</label>
                 </div>
-                <div className='p-4'>
+                <form className='p-4' onSubmit={joinFunction}>
                     <input type="text" name="roomId" className=' w-full h-12 rounded-full border-solid p-4' placeholder='Please enter room id' onChange={onChangeIdRoom} />
-                    <button type='submit' className='text-center w-full mt-5 h-12 rounded-full bg-indigo-600 text-white text-2xl' onClick={joinFunction}>Join</button>
-                </div>
+                    <button type='submit' className='text-center w-full mt-5 h-12 rounded-full bg-white text-black text-2xl' onClick={joinFunction}>Join</button>
+                </form>
+                <a href='https://github.com/hoangminh981/draw-real-time'>
+                    <img src={Github} className=" w-12 text-center ml-42%" alt="logo" />
+                </a>
             </div>
         </div>
     );
