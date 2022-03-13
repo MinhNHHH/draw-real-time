1c098a6afbfe2d1d4019ca154c3894fbfa3f4762
Minh Hoang <hoangminh98.hn2@gmail.com>
2022-03-13 12:13:20 +0700

    Change interface home page

--- client/src/Page/Home.tsx
+++ client/src/Page/Home.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import Github from "../icon/github.svg";
+import  { useState } from "react";
+import { ReactComponent as Github } from "../icon/github.svg";
+import { ReactComponent as Pencil } from "../icon/pencil.svg";
 function Home() {
   const [idRoom, setIdRoom] = useState(null);
 
@@ -15,32 +16,35 @@ function Home() {
   };
 
   return (
-    <div className="bg-cover bg-center bg-gray-600 h-screen">
-      <div className="absolute place-items-center h-2/5 w-378px left-39% bg-cyan-300 top-29% rounded-3xl">
-        <p className="text-center text-3xl top-5 m-7  text-black ">
-          Draw Real Time
-        </p>
+    <div className="bg-cover bg-center h-screen bg-orange-100">
+      <div className="absolute place-items-center h-fit w-378px left-39% top-29% rounded-3xl">
+        <div className="text-center text-5xl font-bold top-5 text-black flex">
+          <div>
+            <Pencil width={50} height={50}/>
+          </div>
+          <p>Draw Real Time</p>
+        </div>
         <div className=" mt-8 text-3xl p-4 text-black">
           <label htmlFor="roomId">Room ID</label>
         </div>
-        <form className="p-4" onSubmit={joinFunction}>
+        <form className="p-4 flex" onSubmit={joinFunction}>
           <input
             type="text"
             name="roomId"
-            className=" w-full h-12 rounded-full border-solid p-4"
+            className=" w-full h-12 border-solid p-4 border-2"
             placeholder="Please enter room id"
             onChange={onChangeIdRoom}
           />
           <button
             type="submit"
-            className="text-center w-full mt-5 h-12 rounded-full bg-white text-black text-2xl"
+            className="text-center w-full h-12  bg-cyan-400 text-black text-2xl"
             onClick={joinFunction}
           >
             Join
           </button>
         </form>
         <a href="https://github.com/hoangminh981/draw-real-time">
-          <img src={Github} className=" w-12 text-center ml-42%" alt="logo" />
+          <Github className=" w-12 text-center ml-42%"/>
         </a>
       </div>
     </div>
