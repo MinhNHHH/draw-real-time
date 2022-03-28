604ca81c2dbd395dfac25f99ef66d468abf99b41
Minh Hoang <hoangminh98.hn2@gmail.com>
2022-03-28 22:51:34 +0700

    replace endpoint

--- client/src/Page/Board.tsx
+++ client/src/Page/Board.tsx
@@ -45,10 +45,9 @@ function Board() {
   const [redoStack, setRedoStack] = useState<Array<any>>([]);
   const [undoStack, setUndoStack] = useState<Array<any>>([]);
   useEffect(() => {
-    // const onSocket = new WebSocket(
-    //   `wss://draw-realtime-socket.herokuapp.com/${id}`
-    // );
-    const onSocket = new WebSocket(`ws://localhost:8000/${id}`);
+    const onSocket = new WebSocket(
+      `wss://${process.env.REACT_APP_BASE_API}/${id}`
+    );
     setSocket(onSocket);
     onSocket.onopen = () => {
       console.log("WebSocket open");
